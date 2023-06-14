4a12e0d07af12c84ef46456002a0156fc3c5778c
yuji061 <136393041+yuji061@users.noreply.github.com>
2023-06-14 14:01:38 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function toggleText() {
+    var x = document.getElementById("ans");
+    var icon = document.getElementById("icon");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        console.log("test");
+        icon.className = "fa fa-chevron-up";
+
+    } else {
+        x.style.display = "none";
+        icon.className = "fa fa-chevron-down";
+    };
+}
+
+function toggleTex() {
+    var x = document.getElementById("answ");
+    var icon = document.getElementById("icone");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        console.log("test");
+        icon.className = "fa fa-chevron-up";
+
+    } else {
+        x.style.display = "none";
+        icon.className = "fa fa-chevron-down";
+    };
+}
+
+function toggle() {
+    var x = document.getElementById("answe");
+    var icon = document.getElementById("icones");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        console.log("test");
+        icon.className = "fa fa-chevron-up";
+
+    } else {
+        x.style.display = "none";
+        icon.className = "fa fa-chevron-down";
+    };
+}
+function toggl() {
+    var x = document.getElementById("answers");
+    var icon = document.getElementById("iconess");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        console.log("test");
+        icon.className = "fa fa-chevron-up";
+
+    } else {
+        x.style.display = "none";
+        icon.className = "fa fa-chevron-down";
+    };
+}
